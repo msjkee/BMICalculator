584d5fae027003206d39d969115c558e91e82b04
Misha (MacBook) <stukalov.2004@inbox.ru>
2025-04-17 22:49:27 +0100

    Const values are applied to colors of widgets

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+const bottomContainerHeight = 80.0;
+const activeCardColor = Color(0xFF1D1E33);
+const bottomContainerColor = Color(0xFFEB1555);
+
 class InputPage extends StatefulWidget {
   const InputPage({super.key});
 
@@ -20,28 +24,34 @@ class _InputPageState extends State<InputPage> {
               child: Row(
                 children: <Widget> [
                   Expanded(
-                    child: ReusableCard(color: Color(0xFF1D1E33))
+                    child: ReusableCard(color: activeCardColor)
                   ),
                   Expanded(
-                    child: ReusableCard(color: Color(0xFF1D1E33))
+                    child: ReusableCard(color: activeCardColor)
                   )
                 ],
               )
           ),
           Expanded(
-            child: ReusableCard(color: Color(0xFF1D1E33))
+            child: ReusableCard(color: activeCardColor)
           ),
           Expanded(
             child: Row(
               children: <Widget> [
                 Expanded(
-                  child: ReusableCard(color: Color(0xFF1D1E33))
+                  child: ReusableCard(color: activeCardColor)
                 ),
                 Expanded(
-                  child: ReusableCard(color: Color(0xFF1D1E33))
+                  child: ReusableCard(color: activeCardColor)
                 )
               ],
             )
+          ),
+          Container(
+            color: bottomContainerColor,
+            margin: EdgeInsets.only(top: 10.0),
+            width: double.infinity,
+            height: bottomContainerHeight,
           )
         ],
       ),
