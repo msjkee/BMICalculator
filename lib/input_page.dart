f33a73b0e7cf9523ea863aeeb7dcf33f85e0e6fe
Misha (MacBook) <stukalov.2004@inbox.ru>
2025-05-03 00:29:06 +0100

    Made my custom FloatingActionButton. It was composed from RawMaterialButton with custom settings. Weight and Age fields are done

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -20,6 +20,8 @@ class _InputPageState extends State<InputPage> {
 
   Gender? selectedGender;
   int height = 180;
+  int weight = 60;
+  int age = 18;
 
   @override
   Widget build(BuildContext context) {
@@ -116,10 +118,86 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: <Widget> [
                 Expanded(
-                  child: ReusableCard(color: kActiveCardColor)
+                  child: ReusableCard(
+                      color: kActiveCardColor,
+                      cardChild: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget> [
+                          Text(
+                            'WEIGHT',
+                            style: kLabelTextStyle
+                          ),
+                          Text(
+                              weight.toString(),
+                            style: kNumberTextStyle
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget> [
+                              RoundIconButton(
+                                icon: FontAwesomeIcons.minus,
+                                onPressed: () {
+                                  setState(() {
+                                    weight--;
+                                  });
+                                },
+                              ),
+                              SizedBox(
+                                width: 10.0
+                              ),
+                              RoundIconButton(
+                                icon: FontAwesomeIcons.plus,
+                                onPressed: () {
+                                  setState(() {
+                                    weight++;
+                                  });
+                                },
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                  )
                 ),
                 Expanded(
-                  child: ReusableCard(color: kActiveCardColor)
+                  child: ReusableCard(
+                      color: kActiveCardColor,
+                      cardChild: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget> [
+                          Text(
+                            'AGE',
+                            style: kLabelTextStyle
+                          ),
+                          Text(
+                            age.toString(),
+                            style: kNumberTextStyle
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget> [
+                              RoundIconButton(
+                                icon: FontAwesomeIcons.minus,
+                                onPressed: () {
+                                  setState(() {
+                                    age--;
+                                  });
+                                },
+                              ),
+                              SizedBox(width: 10.0),
+                              RoundIconButton(
+                                icon: FontAwesomeIcons.plus,
+                                onPressed: () {
+                                  setState(() {
+                                    age++;
+                                  });
+                                },
+                              )
+                            ]
+                          )
+                        ],
+                      ),
+                  )
                 )
               ],
             )
@@ -132,6 +210,28 @@ class _InputPageState extends State<InputPage> {
           )
         ],
       ),
+    );
+  }
+}
+
+class RoundIconButton extends StatelessWidget {
+  final IconData? icon;
+  final void Function()? onPressed;
+
+  RoundIconButton({required this.icon, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+        child: Icon(icon),
+        onPressed: onPressed,
+        shape: CircleBorder(),
+        elevation: 8.0,
+        constraints: BoxConstraints.tightFor(
+            width: 56.0,
+            height: 56.0,
+        ),
+      fillColor: Color(0xFF4C4F5E),
     );
   }
 }
