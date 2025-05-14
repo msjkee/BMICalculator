574638ff9278229a2063ba58251ff4de1720930c
Misha (MacBook) <stukalov.2004@inbox.ru>
2025-05-15 00:59:12 +0100

    Results page is finally well designed. Need to add final logic of functionality into calculator

--- lib/results_page.dart
+++ lib/results_page.dart
@@ -1,6 +1,7 @@
 import 'package:bmi_calculator/constants.dart';
 import 'reusable_card.dart';
 import 'package:flutter/material.dart';
+import 'bottom_button.dart';
 
 class ResultsPage extends StatelessWidget {
   const ResultsPage({super.key});
@@ -16,52 +17,42 @@ class ResultsPage extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget> [
           Expanded(
-            flex: 1,
             child: Container(
-              child: Text('Your Result', style: kResultLabelTextStyle)
+              child: Text('Your Result', style: kTitleTextStyle),
+              padding: EdgeInsets.all(15.0),
+              alignment: Alignment.bottomLeft,
             )
           ),
           Expanded(
-            flex: 8,
+            flex: 5,
             child: ReusableCard(
               color: kActiveCardColor,
               cardChild: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget> [
                   Text(
                     'OVERWEIGHT',
-                    style: TextStyle(
-                      color: Colors.green,
-                      fontSize: 15.0,
-                      fontWeight: FontWeight.bold
-                    )
+                    style: kResultTextStyle
                   ),
                   Text(
                     '26.7',
-                    style: kNumberTextStyle
+                    style: kBMITextStyle
                   ),
                   Text(
                     'You have a higher than normal body weight. Try to exercise more',
-                    style: TextStyle(
-                      fontWeight: FontWeight.normal,
-                      fontSize: 25.0
-                    )
+                    style: kBodyTextStyle,
+                    textAlign: TextAlign.center,
                   ),
                 ]
               ),
             )
           ),
-          Expanded(
-            flex: 1,
-            child: Container(
-              color: kBottomContainerColor,
-              child: Center(
-                child: Text(
-                  'RE-CALCULATE',
-                  style: kLargeButtonTextStyle
-                ),
-              )
-            )
+          BottomButton(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              buttonTitle: 'RE-CALCULATE'
           )
         ]
       )
